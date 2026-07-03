18fbdf260f587d785542e80a3a1d81dff40b0696
Dev1 <dev1@geniusmindzone.com>
2026-07-03 12:26:49 +0500

    Fix scripts

--- scripts/rollback.sh
+++ scripts/rollback.sh
@@ -3,7 +3,7 @@
 
 set -euo pipefail
 
-COMPOSE="docker-compose --env-file .env.production"
+COMPOSE="docker compose --env-file .env.production"
 
 if [ -f .env.production ]; then
   # shellcheck disable=SC1091
